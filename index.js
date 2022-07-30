8a730ac2a0ca222ea8755a89546d8fa86decfff0
Alberto Castro <Alberto1973@live.com>
2022-07-30 17:57:18 -0400

    commit tree 2b359cac4b7abed75b6f661aa77a9039e96fa37a

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase("WELCOME TO FLATBOOK, ");
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
